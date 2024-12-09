f28a6b15f040ac1a35fd043d83db824330a80294
Emiliano Popoca Bolio <emilianopb92@gmail.com>
2024-12-09 01:08:03 -0600

    fix() adds ioredis for connection to redis in heroku

--- src/app/libraries/bullmq/bullmq.module.ts
+++ src/app/libraries/bullmq/bullmq.module.ts
@@ -1,20 +1,25 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
 import { CALLS_WEBHOOKS_QUEUE } from 'src/app/shared/constants';
+import { Redis } from 'ioredis';
 
+const redisOptions =
+  process.env.NODE_ENV === 'development'
+    ? {
+        host: 'localhost',
+        port: 6379,
+      }
+    : new Redis(process.env.REDIS_URL as string, {
+        maxRetriesPerRequest: null,
+        tls: { rejectUnauthorized: process.env.NODE_ENV === 'development' },
+      });
 @Module({
   imports: [
     BullModule.forRootAsync({
       useFactory: () => ({
         connection: {
           url: process.env.REDIS_URL ?? 'redis://localhost:6379',
-          redis: {
-            checkServerIdentity: () => undefined, //bypass hostname validation
-            tls:
-              process.env.NODE_ENV === 'development'
-                ? undefined
-                : { rejectUnauthorized: false },
-          },
+          redisOptions,
         },
       }),
     }),
